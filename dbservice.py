7c0d7a93b5fbb48d55882811772b3a56be6c679a
Ropy Kevin <ropykevin@gmail.com>
2023-10-02 08:38:56 -0900

    update

--- dbservice.py
+++ dbservice.py
@@ -14,6 +14,7 @@ def get_data(table_name):
 
 def insert_products(conn):
     try:
+        
         cursor = conn.cursor()
         product_name = request.form["product_name"]
         buying_price = request.form["buying_price"]
